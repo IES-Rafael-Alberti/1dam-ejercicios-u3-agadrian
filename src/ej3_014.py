068fc9a78eec50eabc6184aacabdc758c9f195f8
aarrgom935 <aarrgom935@g.educaand.es>
2023-11-17 13:22:06 +0100

    push

---
+++ src/ej3_014.py
@@ -0,0 +1,28 @@
+"""
+Ejercicio 3.2.1
+
+Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+"""
+
+
+def comprobarDivisa(diccionario: dict, divisa) -> bool:
+    if divisa not in diccionario:
+        return False
+    else:
+        return True
+
+
+
+def main():
+    diccionario = {'euro':'€', 'dollar':'$', 'yen':'¥'}
+    divisa = input("Introduce divisa a comprobar: ").lower()
+    
+    if comprobarDivisa(diccionario, divisa):
+        print("Tu divisa esta en el diccionario")
+    else:
+        print("No esta en el diccionario")
+    
+
+
+if __name__ == "__main__":
+    main()
