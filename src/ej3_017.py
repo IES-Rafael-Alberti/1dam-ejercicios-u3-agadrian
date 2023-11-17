c3a5c7fc12791a979ca5ff55944ebf22c62da44b
aarrgom935 <aarrgom935@g.educaand.es>
2023-11-17 14:35:05 +0100

    .

--- src/ej3_017.py
+++ src/ej3_017.py
@@ -4,8 +4,18 @@ Ejercicio 3.2.4
 Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre por pantalla la misma fecha en formato dd de <mes> de aaaa donde <mes> es el nombre del mes.
 """
 
+def formatFecha(fecha, mes):
+    
+
 def main():
-    pass
+    fecha = input("Introduce fecha en fromato (dd/mm/aaaa)")
+    dia, mes, año = fecha.split("/")
+    
+    fecha = {
+        "dia": dia,
+        "mes": ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"],
+        "año": año    
+        }
   
 
 if __name__ == "__main__":
