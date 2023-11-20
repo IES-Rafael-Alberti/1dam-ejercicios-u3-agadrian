6c0bce385a98820d1b9a00ef0400e7fda457135a
agadrian <agadrian@iesrafaelalberti.es>
2023-11-20 19:46:14 +0100

    .

--- src/ej3_022.py
+++ src/ej3_022.py
@@ -5,26 +5,63 @@ Escribir un programa que gestione las facturas pendientes de cobro de una empres
 
 
 
-def añadirFactura(diccionario:dict):
+def añadirFactura(diccionario:dict) -> dict:
     numFactura = int(input("Numero factura: "))
-    coste = int(input("Coste factura: "))
+    coste = float(input("Coste factura: "))
     
+    diccionario[numFactura] = coste
+
+    return diccionario
+
+
+
+def pagarFactura(diccionario:dict, diccionarioPagadas:dict):
+
+    numFactura = int(input("Numero de factura a pagar: "))
+    diccionarioPagadas[numFactura] = diccionario[numFactura]
+    diccionario.pop(numFactura)
+
+    # En este caso no seria necesario retornan estos valores, ya que los cambios realizados se ehecutan una vez llamada la funcion, sin necesidad de retornarlos
+    return diccionario, diccionarioPagadas
+
+
+
+def mostrarCobros(diccionario:dict, diccionarioPagadas:dict):
+    
+    totalPagado = sum(diccionarioPagadas.values())
+    totalPorPagar = sum(diccionario.values())
+
+    print(f"Total pagado {totalPagado}€. Por pagar {totalPorPagar}€.")
 
 
 
 
-
+def mostrarDiccionario(diccionario:dict):
+    print("\nNumero Factura -- Cantidad\n")
+    for k,v in diccionario.items():
+        print(f"{k}  --  {v}€")
+    
 
 def main():
     diccionario = {}
-    opc = int(input("Elije una opcion (1/Añadir factura, 2/Pagar factura existente, 3/Terminar): "))
+    diccionarioPagadas = {}
+    ok = False
 
-    if opc == 1:
-        añadirFactura(diccionario)
+    while not ok:
+        opc = int(input("Elije una opcion (1/Añadir factura, 2/Pagar factura existente, 3/Terminar): "))
 
-
-
-
+        if opc == 1:
+            añadirFactura(diccionario)
+            mostrarDiccionario(diccionario)
+            mostrarCobros(diccionario, diccionarioPagadas)
+        elif opc == 2:
+            pagarFactura(diccionario, diccionarioPagadas)
+            mostrarDiccionario(diccionario)
+            mostrarCobros(diccionario, diccionarioPagadas)
+        else:
+            ok = True
+            mostrarDiccionario(diccionario)
+            mostrarCobros(diccionario, diccionarioPagadas)
 
 
 
